b571a239d9117a0de16a9494866fce7fe26dc114
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-12 19:00:05 +0200

    es3

--- js/script.js
+++ js/script.js
@@ -43,3 +43,45 @@ const btn2 = function(){
 
 }
 button2.addEventListener('click', btn2);
+
+const button3 = document.querySelector('#btn3');
+let arrayRandom = [];
+const btn3 = function(){
+    const result3 = document.querySelector('#result3-1');
+    result3.innerHTML = '';
+    const result4 = document.querySelector('#result3-2');
+    result4.innerHTML = '';
+    let numberMax = document.querySelector('#number3-1').value;
+    let numbersToGenerate = document.querySelector('#number3-2').value;
+    if (numbersToGenerate > numberMax){
+        result3.innerHTML = 'Inserisci un massimo più grande'
+    }else{
+        for (let i = 1; i <= numbersToGenerate; i++) {
+            let check = false;
+            let generatedNumber = Math.floor(Math.random() * numberMax);
+            for (let j = 0; j < arrayRandom.length; j++) {
+                if (generatedNumber == arrayRandom[j]) {
+                    check = true;
+                    i--;
+                }
+            }
+            if (!check) {
+                arrayRandom.push(generatedNumber);
+            }
+        }
+        let sum = 0;
+        console.log(arrayRandom);
+        for(let i = 0; i<arrayRandom.length; i++){
+            if(i%2 != 0){
+                sum += arrayRandom[i];
+            } 
+        }
+        let text = arrayRandom.toString();
+        result3.innerHTML = 'La lista creata è: ' + text;
+        result4.innerHTML = 'La somme degli indici dispari è: ' + sum;
+        arrayRandom =[];
+    }
+    
+}
+button3.addEventListener('click', btn3);
+
